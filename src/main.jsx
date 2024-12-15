8d398f160dcf70c7b5f6e8834894662f0965f294
touhidul-islam-html <ti360109@gmail.com>
2024-12-15 09:35:05 +0600

    updadte

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,13 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
-import PayFee from "./Pages/PayFee";
 import Dashboard from "./StudentPanel/Dashboard";
 import Login from "./StudentPanel/Login";
 import Auth from "./Auth/Auth";
 import NoticeBoard from "./StudentPanel/NoticeBoard";
 import Result from "./StudentPanel/Result";
 import IdCard from "./StudentPanel/IdCard";
+import PayFee from "./StudentPanel/PayFee";
 
 const router = createBrowserRouter([
   {
