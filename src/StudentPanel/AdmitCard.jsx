f0905c1352b0905a7086e224d476ced79a52285d
touhidul-islam-html <ti360109@gmail.com>
2024-12-23 12:37:09 +0600

    update

--- src/StudentPanel/AdmitCard.jsx
+++ src/StudentPanel/AdmitCard.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable react/no-unescaped-entities */
 import { useOutletContext } from "react-router-dom";
@@ -6,15 +7,37 @@ import jsPDF from "jspdf";
 import "./testimonial.css";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
 
 const AdmitCard = () => {
   const { data } = useOutletContext();
   const [classes, setClasses] = useState(null);
-  console.log(classes);
+  const [formData, setFormData] = useState();
+  const [terms, setTerms] = useState([]);
+
   const backendApiUrl = import.meta.env.VITE_API_BASE_URL;
-  /* Pdf Download */
+ 
+ /* Term Name Input */
+ const handleInputChange = (e) => {
+  const { name, value } = e.target;
+  setFormData((prevData) => ({
+    ...prevData,
+    [name]: value,
+  }));
+};
+ 
+/* Fetch Data From Backend */
 
   useEffect(() => {
+    axios
+     .get(`${backendApiUrl}/getExamName`)
+     .then(function (response) {
+       setTerms(response.data.data);
+     })
+     .catch(function (error) {
+       console.log(error);
+       toast.error("Result Not Found");
+     });
     axios
       .get(`${backendApiUrl}/getCourse/${data.classname}`)
       .then(function (response) {
@@ -59,23 +82,70 @@ const AdmitCard = () => {
   /*  */
   return (
     <div className=" min-h-screen flex flex-col items-center justify-center">
+       <div className="px-10 py-10 lg:w-[70%] m-auto">
+        <form>
+          <FormSection title="Select Exam Term">
+            <div>
+              <label htmlFor="classname" className="block mb-1">
+                Select Exam Terms
+              </label>
+              <select
+                name="terms"
+                id="terms"
+                className="w-full border rounded px-2 py-1"
+                onChange={handleInputChange}
+              >
+                <option value="">Select Exams Terms</option>
+                {terms?.map((item) => (
+                  <option key={item.id} value={item.name}>
+                    {item.name}
+                  </option>
+                ))}
+              </select>
+            </div>
+          </FormSection>
+          {/* <button
+            type="submit"
+            className="bg-blue-500 text-white px-6 py-2 rounded hover:bg-blue-600 w-full"
+          >
+            Search Result
+          </button> */}
+        </form>
+      </div>
       <div className="w-[1000px] flex flex-col justify-between h-[710px] mx-auto mt-5 border-[15px] border-[#15803D] p-10 bg-transparent shadow-lg relative testimonial">
         <div className="absolute inset-[15px] border-2 border-[#15803D] -z-10"></div>
         <div>
           <div className="text-center mb-2">
-            <img
-              src="./logo.jpg"
-              alt="Bangladesh Logo"
-              className="w-[70px] mx-auto mb-3"
-            />
-            <h1 className="text-green-700 text-[1.8rem] font-semibold my-1">
-              মেধা বিকাশ শিশু নিকেতন এন্ড কুরআন একাডেমি
+            {/* <img
+            src="./logo.jpg"
+            alt="Bangladesh Logo"
+            className="w-[70px] mx-auto mb-3"
+          /> */}
+            <h1 className="text-[#2DB75B] text-[30px] leading-tight font-semibold m-0 p-0 -mt-4">
+              মেধা বিকাশ শিশু নিকেতন এন্ড কোরআন একাডেমি
             </h1>
-            <h2 className="text-purple-700 text-[1.3rem] font-medium">
-              প্রবেশ পত্র
-            </h2>
+            <h1 className="text-[#1A56DB] text-[22px] font-semibold -mt-2">
+              Medha Bikash Shisho Niketan And Quran Academy
+            </h1>
+            <div className="text-center  font-[450] text-md">
+              <p>
+                স্থাপিতঃ ২০<span className="font-poppins">১</span>৮ ইং,
+              </p>
+              <p>বিদ্যালয় কোডঃ ৭০৫০৮৩৩২০</p>
+              <p>৩৩ নং ওয়ার্ড মহানগর, রংপুর।</p>
+            </div>
+            
           </div>
           <div className="idcard-content">
+          <div className="text-center flex flex-col items-center justify-center">
+              <p className="text-center text-xl font-semibold">
+                {formData?.terms}
+                
+              </p>
+              <h2 className="text-purple-700 text-[1.3rem] w-fit font-medium text-center pt-1 px-5 rounded-md ">
+                প্রশংসা পত্র
+              </h2>
+            </div>
             <div className="flex w-full justify-between">
               {/* Information Col 1 */}
               <div className=" flex flex-col justify-start w-[40%] gap-2">
@@ -103,8 +173,7 @@ const AdmitCard = () => {
               {/* Information Col 2 */}
 
               <div className=" flex flex-col justify-start w-[40%] gap-2">
-
-              <div className="flex items-center">
+                <div className="flex items-center">
                   <span className="w-1/2 font-bold">ID</span>
                   <span className="w-1/2 text-left">
                     <b>:</b> {data.studentId}
@@ -124,14 +193,13 @@ const AdmitCard = () => {
                     <b>:</b> {data.classname}
                   </span>
                 </div>
-
               </div>
             </div>
           </div>
         </div>
 
         <div className="subjects-section flex flex-col justify-center w-full">
-          <div className="heading text-center text-2xl font-bold py-4">
+          <div className="heading text-center text-xl font-bold pb-4">
             NAME OF SUBJECTS
           </div>
           {/* Make the grid box contents center course*/}
@@ -141,6 +209,7 @@ const AdmitCard = () => {
                 {courses.course}
               </div>
             ))}
+           
           </div>
         </div>
 
@@ -151,10 +220,6 @@ const AdmitCard = () => {
               <p>
                 1. The Examinee must bring the Admit Card in the Examination
                 Hall and without any electronic device.
-              </p>
-              <p>
-                2. The examinee must sign the attendance sheet for each subject,
-                or they will be marked absent.
               </p>
             </div>
           </div>
@@ -184,3 +249,9 @@ const AdmitCard = () => {
 };
 
 export default AdmitCard;
+const FormSection = ({ title, children }) => (
+  <fieldset className="border border-green-600 p-4 mb-4 flex flex-col justify-end">
+    <legend className="px-2 text-lg text-green-700">{title}</legend>
+    <div className="grid grid-cols-1 gap-4">{children}</div>
+  </fieldset>
+);
